84da687b4019560fed37ae8906f645a073208994
kazuhi-ra <i-hashizume@adventure-inc.co.jp>
2021-02-03 14:45:32 +0900

    redux-toolkitの導入して動くまでやった

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { counterSlice, CounterState } from './feature/counter'
 
-function App() {
+const App = () => {
+  const count = useSelector<CounterState, number>((state) => state.count)
+  const dispatch = useDispatch()
+  const { added, decremented, incremented } = counterSlice.actions
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>count: {count}</div>
+      <button onClick={() => dispatch(incremented())}>+</button>
+      <button onClick={() => dispatch(decremented())}>-</button>
+      <button onClick={() => dispatch(added(10))}>+10</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/feature/counter.ts
@@ -0,0 +1,17 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export type CounterState = { count: number }
+const initialState: CounterState = { count: 10 }
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    incremented: (state) => ({ ...state, count: state.count + 1 }),
+    decremented: (state) => ({ ...state, count: state.count - 1 }),
+    added: (state, action: PayloadAction<number>) => ({
+      ...state,
+      count: state.count + action.payload,
+    }),
+  },
+})
